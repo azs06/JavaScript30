2d1976fa5dbb3a54ef38f8476ebe266641edb022
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2016-12-31 17:00:23 +0600

    added select feature on type ahead drop down

--- 06 - Type Ahead/script.js
+++ 06 - Type Ahead/script.js
@@ -1,9 +1,6 @@
     /*
     
     @todo
-    add babel to make it cross browser compatible
-    make the dropdown selectable
-    
     */
 	(function(){
 	"use strict";
@@ -11,7 +8,7 @@
 	const searchInput = document.querySelector('.search');
     const suggestions = document.querySelector('.suggestions');
     const suggestionsHolder = `<li>Filter for a city</li><li>or a state</li>`;
-    suggestions.innerHTML = suggestionsHolder;
+    suggestions.innerHTML = suggestionsHolder;	
     const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
     const cities = [];
     /*using fetch api to fetch data and append it to an array*/
@@ -29,28 +26,47 @@
             return place.city.match(regex) || place.state.match(regex);
         });
     }
-    
+    function initSelector(){	
+		console.log(this.dataSet);
+	}
     function displayMatches(){
+		if(searchInput.value === ''){
+			suggestions.classList.remove('searchList--open');
+			suggestions.innerHTML = suggestionsHolder
+			return;
+        }
         const matchArray = findMatchWord(this.value, cities);
         const html = matchArray.map((place)=>{
             const regex = new RegExp(this.value, 'gi');
             const cityName = place.city.replace(regex, `<span class="hl">${place.city}</span>`);
             const stateName = place.state.replace(regex, `<span class="hl">${place.state}</span>`);
-            return `<li data-city="${place.city}" data-state="${place.state}">
+			
+            return `<li class="areaData" data-city="${place.city}" data-state="${place.state}">
                     <span class="name"> ${cityName}, ${stateName} </span>   
                     <span class="polpulation"> ${numberWithCommas(place.population)} </span>    
                     </li>`;
         }).join('');
-        
-        if(searchInput.value === ''){
-            suggestions.innerHTML = suggestionsHolder
-            return;
-        }
-        
+		suggestions.classList.add('searchList--open');
         suggestions.innerHTML = html;
+		const suggestionsLi = suggestions.querySelectorAll('li');
+			suggestionsLi.forEach(function(area){
+				area.addEventListener('click', function(){
+					updateInput(this.dataset);
+			})
+		});	
+		
     }
-    
-    searchInput.addEventListener('change',displayMatches);
-    searchInput.addEventListener('keyup',displayMatches);	
+	
+	function updateInput(newData){
+		searchInput.value = newData.city + ", " + newData.state;
+		
+	}	
+
+	window.onload = function(){
+		searchInput.addEventListener('change',displayMatches);
+		searchInput.addEventListener('keyup',displayMatches);
+	
+	}	
+		
 	}())
     
